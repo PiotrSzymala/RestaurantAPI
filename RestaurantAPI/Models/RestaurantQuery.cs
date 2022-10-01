27f7b33ced66d7f8fd5de7fba98a70de60d8ddd8
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-10-01 02:45:37 +0200

    Add results sorting

--- RestaurantAPI/Models/RestaurantQuery.cs
+++ RestaurantAPI/Models/RestaurantQuery.cs
@@ -11,5 +11,7 @@ namespace RestaurantAPI.Models
         public string SearchPhrase { get; set; }
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 5;
+        public string SortBy { get; set; }
+        public SortDirection SortDirection { get; set; }
     }
 }
--- RestaurantAPI/Models/Validator/RestaurantQueryValidator.cs
+++ RestaurantAPI/Models/Validator/RestaurantQueryValidator.cs
@@ -4,22 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
+using RestaurantAPI.Entities;
 
 namespace RestaurantAPI.Models.Validator
 {
     public class RestaurantQueryValidator : AbstractValidator<RestaurantQuery>
     {
-        private int[] allowedPageSizes = new[] { 5, 10, 15 };
+        private readonly int[] _allowedPageSizes = new[] { 5, 10, 15 };
+        private string[] _sortByColumnNames = new[] { nameof(Restaurant.Name), nameof(Restaurant.Category), nameof(Restaurant.Description)};
         public RestaurantQueryValidator()
         {
             RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
             RuleFor(r => r.PageSize).Custom((value, context) =>
             {
-                if (!allowedPageSizes.Contains(value))
+                if (!_allowedPageSizes.Contains(value))
                 {
-                    context.AddFailure("PageSize", $"PageSize must in [{string.Join(",",allowedPageSizes)}]");
+                    context.AddFailure("PageSize", $"PageSize must in [{string.Join(",",_allowedPageSizes)}]");
                 }
             });
+            RuleFor(r => r.SortBy).Must(value=>string.IsNullOrEmpty(value) || _sortByColumnNames.Contains(value))
+                .WithMessage($"Sort by is optional, or must be in [{string.Join(",", _sortByColumnNames)}]");
         }
     }
 }
