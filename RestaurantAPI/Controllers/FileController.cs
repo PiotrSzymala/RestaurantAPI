d3afab8c56ebe04ad9e429d05d3d35f87978fba1
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2022-10-01 17:54:23 +0200

    Add file upload method

--- RestaurantAPI/Controllers/FileController.cs
+++ RestaurantAPI/Controllers/FileController.cs
@@ -13,6 +13,7 @@ namespace RestaurantAPI.Controllers
     [Authorize]
     public class FileController : ControllerBase
     {
+        [HttpGet]
         public ActionResult GetFile([FromQuery] string fileName)
         {
             var rootPath = Directory.GetCurrentDirectory();
@@ -32,6 +33,26 @@ namespace RestaurantAPI.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm] IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.Name;
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
